c02ecac8a34b6c5b43cca3bd0d3e731a297ddb88
Lovell-Hunter-2 <ngothuanbg1612007@gmail.com>
2026-05-28 14:52:50 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -204,7 +204,7 @@ export default function App() {
           } catch (e) {}
           let periods = [1, 2, 3];
           const timeStr = String(item.examTime || '');
-          const shiftStr = String(item.examHour || item.shift || item.caThi || '');
+          const shiftStr = String(item.examShift || item.shift || item.caThi || '');
           let shiftMatch = shiftStr.match(/^(\d+)(?:\s*-\s*(\d+))?$/);
           
           if (shiftMatch) {
