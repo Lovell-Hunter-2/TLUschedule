2b401f0d8d0c434ec3eb8a3a6efd98b3d5e2caf8
Lovell-Hunter-2 <ngothuanbg1612007@gmail.com>
2026-07-28 21:33:13 +0700

    Refactor Firestore setup with persistent local cache
    Updated Firestore initialization to use new methods for local cache and tab management.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'; // Thêm import này
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const googleProvider = new GoogleAuthProvider();
-
-// BẬT CHẾ ĐỘ OFFLINE
-enableIndexedDbPersistence(db).catch((err) => {
-  console.log("Lỗi bật chế độ offline: ", err.message);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({tabManager: persistentMultipleTabManager()}),
+  databaseId: firebaseConfig.firestoreDatabaseId
 });
+export const googleProvider = new GoogleAuthProvider();
